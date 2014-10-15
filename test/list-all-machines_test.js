0cece89b7ce89d13fd28190e9a3a552b6d4f1c84
guym <guym@gigaspaces.com>
2014-10-15 15:05:05 +0300

    fix(build): fix build

--- test/list-all-machines_test.js
+++ test/list-all-machines_test.js
@@ -4,7 +4,7 @@ var assert = require('assert'),
   listAllMachines = require('../lib/list-all-machines.js');
 
 describe('list-all-machines node module.', function() {
-  it('must be awesome', function() {
-    assert( listAllMachines.awesome(), 'awesome');
+  it('should have a list function', function() {
+    assert( typeof(listAllMachines.list), 'function');
   });
 });
